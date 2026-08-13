5ed203eda49b8044f8898cad5e85b3f74497d801
fas_5 <adnanfaseehms@gmail.com>
2026-08-13 18:32:19 +0530

    fix: make wa migration case insensitive for usernames

--- app/utils/migration_wa.py
+++ app/utils/migration_wa.py
@@ -1,23 +1,24 @@
 def run_wa_migration(db):
+    import re
     try:
         # Update Faseeh
         db.users.update_one(
-            {"username": "Faseeh"},
+            {"username": {"$regex": re.compile("^faseeh$", re.IGNORECASE)}},
             {"$set": {"phone": "918056357572"}}
         )
         # Update Hari
         db.users.update_one(
-            {"username": "Hari"},
+            {"username": {"$regex": re.compile("^hari$", re.IGNORECASE)}},
             {"$set": {"phone": "917806847970"}}
         )
         # Update Sudhakaran
         db.users.update_one(
-            {"username": "Sudhakaran"},
+            {"username": {"$regex": re.compile("^sudhakaran$", re.IGNORECASE)}},
             {"$set": {"phone": "919597718611"}}
         )
         # Update Yoganathan
         db.users.update_one(
-            {"username": "Yoganathan"},
+            {"username": {"$regex": re.compile("^yoganathan$", re.IGNORECASE)}},
             {"$set": {"phone": "918072033891"}}
         )
         print("WhatsApp migration complete.")
